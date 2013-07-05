2e2eacf254266d2e1dd41d8520f5d1fa35ac5a34
Ubuntu <ubuntu@ip-10-164-68-233.ec2.internal>
2013-07-05 15:19:57 +0000

    Updated to read string from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var indexFile = 'index.html';
+
+var textString = fs.readFileSync(indexFile).toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(textString);
 });
 
 var port = process.env.PORT || 5000;
