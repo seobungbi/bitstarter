7e2ee9746975deed659be02a3098e6a19f6a8993
Ubuntu <ubuntu@ip-10-164-68-233.ec2.internal>
2013-07-05 19:26:07 +0000

    Updated code to accept URLs from command-line

--- grader.js
+++ grader.js
@@ -25,8 +25,13 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var TMP_HTML_FILE = "tmp-grader.html";
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
@@ -62,14 +67,41 @@ var clone = function(fn) {
   return fn.bind({});
 };
 
+var buildfn = function(checkFile) {
+  var checkURL = function(result, response) {
+    if (result instanceof Error) {
+      console.error('Error: ' + util.format(response.message));
+    } else {
+      fs.writeFileSync(TMP_HTML_FILE, result);
+      console.error("Wrote: %s", TMP_HTML_FILE); 
+
+      var checkJson = checkHtmlFile(TMP_HTML_FILE, checkFile);
+      fs.unlinkSync(TMP_HTML_FILE);
+      console.error("Deleted tmp file");
+
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
+  }
+  return checkURL;
+}
+
 if (require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'Path to web URL')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  var parseFile = program.file;
+  if (program.url) { 
+    var checkURL = buildfn(program.checks);
+    rest.get(program.url).on('complete', checkURL);
+  } else {
+    var checkJson = checkHtmlFile(parseFile, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
